e5ba904d4a028073cf2f8ca7a470c73518c247dd
Nhahan <kisy324@naver.com>
2022-09-12 23:16:20 +0900

    Describe default value of size parameter

--- src/main/java/com/example/books/controller/BookController.java
+++ src/main/java/com/example/books/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.books.controller;
 import com.example.books.dto.BookRequestDto;
 import com.example.books.dto.BookResponseDto;
 import com.example.books.service.BookService;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,9 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    public ResponseEntity<List<BookResponseDto>> getBooks(@RequestParam int page, @RequestParam(required = false , defaultValue = "10") int size) {
+    public ResponseEntity<List<BookResponseDto>> getBooks(
+            @RequestParam int page,
+            @RequestParam(required = false , defaultValue = "10") @ApiParam(value = "default: 10")  int size) {
         return ResponseEntity.ok().body(this.bookService.getBooks(PageRequest.of(page - 1, size)));
     }
 
