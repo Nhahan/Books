f95e793321bbb9df2b0c8f9eb1ef9f205be9554c
Nhahan <kisy324@naver.com>
2022-09-13 00:47:36 +0900

    Fix bug, Add missing @Param

--- src/main/java/com/example/books/repository/AuthorBookRepository.java
+++ src/main/java/com/example/books/repository/AuthorBookRepository.java
@@ -3,10 +3,12 @@ package com.example.books.repository;
 import com.example.books.model.AuthorBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AuthorBookRepository extends JpaRepository<AuthorBook, Long> {
     @Query("select ab from AuthorBook ab left join Author as a on ab.author.id = a.id where ab.book.id = :id")
-    List<AuthorBook> findAllByBookId(Long id);
+    List<AuthorBook> findAllByBookId(@Param("id") Long id);
+
 }
