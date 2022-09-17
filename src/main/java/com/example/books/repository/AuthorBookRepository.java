bb9e0fdcf1d3d3b9d2d15abf8f2d1aea800010f3
Nhahan <kisy324@naver.com>
2022-09-18 00:54:54 +0900

    Fix findAuthorBooks query option

--- src/main/java/com/example/books/repository/AuthorBookRepository.java
+++ src/main/java/com/example/books/repository/AuthorBookRepository.java
@@ -2,14 +2,12 @@ package com.example.books.repository;
 
 import com.example.books.model.AuthorBook;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AuthorBookRepository extends JpaRepository<AuthorBook, Long> {
-    @Modifying
     @Query("select ab from AuthorBook ab left join fetch Author as a on ab.author.id = a.id where ab.book.id in :ids")
     List<AuthorBook> findAllByBookIds(@Param("ids") List<Long> ids);
 
